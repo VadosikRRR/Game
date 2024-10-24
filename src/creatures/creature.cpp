ebab6fc2a3dda15b79c9275f08bbff6bc661014f
artem smola <artemsmola055@gmail.com>
2024-10-24 13:43:14 +0300

    Add position field and move method to creature

--- src/creatures/creature.cpp
+++ src/creatures/creature.cpp
@@ -1,54 +1,72 @@
 #include "creature.hpp"
 
-Creature::Creature(Creature_type type, string name, char designation)
-    : type(type), name(name), designation(designation) {
-  switch (type) {
+Creature::Creature(Creature_type type, string name, char designation, Point pos)
+    : type_(type), name_(name), designation_(designation), position_(pos) {
+  switch (type_) {
   case goblin:
-    health = 25;
-    damage = 5;
-    protection = 5;
+    health_ = 25;
+    damage_ = 5;
+    protection_ = 5;
     break;
   case troll:
-    health = 150;
-    damage = 15;
-    protection = 20;
+    health_ = 150;
+    damage_ = 15;
+    protection_ = 20;
     break;
   case elf_wizard:
-    health = 25;
-    damage = 15;
-    protection = 0;
+    health_ = 25;
+    damage_ = 15;
+    protection_ = 0;
     break;
   case elf_archer:
-    health = 25;
-    damage = 10;
-    protection = 0;
+    health_ = 25;
+    damage_ = 10;
+    protection_ = 0;
     break;
   case merchant:
-    health = 50;
-    damage = 10;
-    protection = 10;
+    health_ = 50;
+    damage_ = 10;
+    protection_ = 10;
     break;
   case villager:
-    health = 5;
-    damage = 1;
-    protection = 0;
+    health_ = 5;
+    damage_ = 1;
+    protection_ = 0;
     break;
   case boss:
-    health = 300;
-    damage = 20;
-    protection = 25;
+    health_ = 300;
+    damage_ = 20;
+    protection_ = 25;
     break;
   }
 }
 
 Creature::~Creature() {}
 
-string Creature::get_name() { return name; }
-Creature_type Creature::get_type() { return type; }
-unsigned int Creature::get_health() { return health; }
-unsigned int Creature::get_damage() { return damage; }
-unsigned int Creature::get_protection() { return protection; }
-char Creature::get_designation() { return designation; }
-void Creature::reduce_health(unsigned int value) {
-  health = (health > value) ? (health - value) : 0;
+string Creature::GetName() { return name_; }
+Creature_type Creature::GetType() { return type_; }
+unsigned int Creature::GetHealth() { return health_; }
+unsigned int Creature::GetDamage() { return damage_; }
+unsigned int Creature::GetProtection() { return protection_; }
+char Creature::GetDesignation() { return designation_; }
+void Creature::ReduceHealth(unsigned int value) {
+  health_ = (health_ > value) ? (health_ - value) : 0;
+}
+
+Point Creature::GetPosition(){ return position_; }
+
+void Creature::Move(int add_x, int add_y, const game::Map & map){
+  int new_x = position_.getX() + add_x;
+  int new_y = position_.getY() + add_y;
+
+  if (new_x >= 0 && new_x < game::kMapWidth && new_y >= 0 && new_y < game::kMapHeight &&
+      map.IsWalkable(new_x, new_y)) {
+    position_.changeX(new_x);
+    position_.changeY(new_y);
+  }
+}
+
+void Creature::SetPosition(const Point & point){
+  position_.changeX(point.getX());
+  position_.changeY(point.getY());
 }
--- src/creatures/creature.hpp
+++ src/creatures/creature.hpp
@@ -1,24 +1,33 @@
+#pragma once
 #include "creature_type.hpp"
+#include "../basic_classes/point.hpp"
+#include "../map/map.h"
 #include <iostream>
 using namespace std;
 
 class Creature {
 private:
-  unsigned int health;
-  unsigned int damage;
-  unsigned int protection;
-  string name;
-  char designation;
-  Creature_type type;
+  unsigned int health_;
+  unsigned int damage_;
+  unsigned int protection_;
+  unsigned int probability_of_hit;
+  string name_;
+  char designation_;
+  Creature_type type_;
+  Point position_;
 
 public:
-  Creature(Creature_type type, string name, char designation);
+  Creature(Creature_type type, string name, char designation, Point pos = Point(0,0));
   virtual ~Creature();
-  string get_name();
-  Creature_type get_type();
-  unsigned int get_health();
-  unsigned int get_damage();
-  unsigned int get_protection();
-  char get_designation();
-  void reduce_health(unsigned int value);
+  string GetName();
+  Creature_type GetType();
+  unsigned int GetHealth();
+  unsigned int GetDamage();
+  unsigned int GetProtection();
+  unsigned int GetProbabilityOfHit();
+  char GetDesignation();
+  Point GetPosition();
+  void Move(int add_x, int add_y, const game::Map &map);
+  void SetPosition(const Point &point);
+  void ReduceHealth(unsigned int value);
 };
