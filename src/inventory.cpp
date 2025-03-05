021bce445a4a1019f7cc4e216f8ee4d53fb8a5ee
Oddin60F <vanyakozhukov2004@gmail.com>
2025-03-05 23:47:12 +0300

    fix error with adding items

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -16,8 +16,10 @@ void Inventory::Add(const std::shared_ptr<Item> &item)
         for (auto &existing_item : container_) {
             std::shared_ptr<CollectiblesItem> existing_collectible
                 = std::dynamic_pointer_cast<CollectiblesItem>(existing_item);
-            if (existing_collectible->GetName().section('(', 0, 0).trimmed()
-                == collectible->GetName().section('(', 0, 0).trimmed()) {
+
+            if (existing_collectible
+                && existing_collectible->GetName().section('(', 0, 0).trimmed()
+                       == collectible->GetName().section('(', 0, 0).trimmed()) {
                 existing_collectible->Add();
                 return;
             }
